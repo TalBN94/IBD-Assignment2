4bc348cbdbf36a369caf36fdc53886bfc09e5876
TalBN94 <talbarnathan@gmail.com>
2021-12-13 18:54:15 +0200

    Changed plots of q2 to match requirements in question

--- softsvm.py
+++ softsvm.py
@@ -2,8 +2,6 @@ import numpy as np
 from cvxopt import solvers, matrix, spmatrix, spdiag, sparse
 import matplotlib.pyplot as plt
 
-solvers.options['show_progress'] = False
-
 
 # todo: complete the following functions, you may add auxiliary functions or define class to help you
 
@@ -17,20 +15,25 @@ def softsvm(l, trainX: np.array, trainy: np.array):
     """
     d = trainX.shape[1]
     m = trainX.shape[0]
+
+    # build H
     Id = spmatrix(2.0*l, range(d), range(d))
     zeros_dm = spmatrix([], [], [], (d, m))
     zeros_md = spmatrix([], [], [], (m, d))
     zeros_mm = spmatrix([], [], [], (m, m))
     H = sparse([[Id, zeros_md], [zeros_dm, zeros_mm]], tc='d')
 
+    # build v
     v1 = matrix(np.array(np.ones((m, 1))))
     v2 = matrix(np.array(np.zeros((m, 1))))
     v = matrix([v1, v2], tc='d')
 
+    # build u
     u1 = matrix(np.array(np.zeros((d, 1))))
     u2 = matrix(np.array((1 / m) * np.ones((m, 1))))
     u = matrix([u1, u2], tc='d')
 
+    # build A
     y_diag = np.diag(trainy.reshape(-1))
     A1 = y_diag @ trainX
     A1 = matrix(np.array(A1))
@@ -38,6 +41,7 @@ def softsvm(l, trainX: np.array, trainy: np.array):
     Im = matrix(np.array(Im))
     A = sparse([[A1, zeros_md], [Im, Im]], tc='d')
 
+    # Run solver and extract w from the solution z
     sol = solvers.qp(H, u, -A, -v)
     z = sol["x"]
     return np.array(z)[:d, :]
@@ -50,8 +54,6 @@ def load_n_data(n):
     testX = data['Xtest']
     trainy = data['Ytrain']
     testy = data['Ytest']
-
-    d = trainX.shape[1]
 
     # Get a random m training examples from the training set
     indices = np.random.permutation(trainX.shape[0])
@@ -71,25 +73,32 @@ def get_error_percentage(pred_labels, real_labels):
     return error_count / len(real_list)
 
 
-def plot_experiment_result(results_train, results_test, lamda_exps, is_small=True):
+def plot_experiment_result(results_train, results_test, lamda_exps, results_train_large=None, results_test_large=None,
+                           lambda_exps_large=None, is_small=True):
     error_mins_train = np.asarray([result[2] for result in results_train])
     error_maxes_train = np.asarray([result[1] for result in results_train])
     error_means_train = np.asarray([result[0] for result in results_train])
     plt.errorbar(lamda_exps, error_means_train, [error_means_train - error_mins_train, error_maxes_train - error_means_train],
-                 fmt='o', lw=1, ecolor='blue')
+                 fmt='o', lw=1, ecolor='blue', zorder=2)
 
     error_mins_test = np.asarray([result[2] for result in results_test])
     error_maxes_test = np.asarray([result[1] for result in results_test])
     error_means_test = np.asarray([result[0] for result in results_test])
     plt.errorbar(lamda_exps, error_means_test, [error_means_test - error_mins_test, error_maxes_test - error_means_test],
-                 fmt='o', lw=1, ecolor='red')
+                 fmt='o', lw=1, ecolor='orange', zorder=2)
 
     if is_small:
-        plt.plot(lamda_exps, error_means_train, 'b')
-        plt.plot(lamda_exps, error_means_test, 'r')
+        plt.legend(['Train result', 'Test results'], loc='upper left')
+    else:
+        error_means_train_large = np.asarray([result[0] for result in results_train_large])
+        error_means_test_large = np.asarray([result[0] for result in results_test_large])
+        plt.scatter(lambda_exps_large, error_means_train_large, color='green', zorder=3)
+        plt.scatter(lambda_exps_large, error_means_test_large, color='purple', zorder=3)
+        plt.legend(['Train result (1000)', 'Test results (1000)', 'Train result (100)', 'Test results (100)'],
+                   loc='upper left')
 
-    plt.legend(['Train result', 'Test results'], loc='upper left')
-
+    plt.plot(lamda_exps, error_means_train, 'blue', zorder=0)
+    plt.plot(lamda_exps, error_means_test, 'orange', zorder=0)
     plt.xticks(lamda_exps, lamda_exps)
     plt.xlabel(r'$\lambda$ (log scale)')
     plt.ylabel('Error rate')
@@ -147,7 +156,7 @@ def simple_test():
 
 if __name__ == '__main__':
     # before submitting, make sure that the function simple_test runs without errors
-    # simple_test()
+    simple_test()
 
     # here you may add any code that uses the above functions to solve question 2
     exps1 = range(1, 11)
@@ -158,4 +167,5 @@ if __name__ == '__main__':
     exps2 = [1, 3, 5, 8]
     ls2 = [10**i for i in exps2]
     results_1000_samples_train, results_1000_samples_test = run_experiment(1000, ls2, 1)
-    plot_experiment_result(results_1000_samples_train, results_1000_samples_test, exps2, is_small=False)
+    plot_experiment_result(results_100_samples_train, results_100_samples_test, exps1,
+                           results_1000_samples_train, results_1000_samples_test, exps2, is_small=False)
